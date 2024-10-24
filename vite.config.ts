5a7a5d4d2f324bea6d4a5dff9ada2f849b93589d
Konosuke Sakai <84666033+sakkke@users.noreply.github.com>
2024-10-25 03:16:40 +0900

    feat(vite.config.ts): add basic SSL plugin to Vite configuration for enhanced security during development

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import preact from "@preact/preset-vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), basicSsl()],
 });
